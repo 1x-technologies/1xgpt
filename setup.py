b3ff34c8171aa824f724107946d44562ae4ee5d4
Eric Jang <eric@1x.tech>
2024-07-09 20:39:34 -0700

    try rename

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
 from setuptools import find_namespace_packages, setup
 
-def parse_requirements(filename):
-    with open(filename) as f:
-        lines = f.read().splitlines()
-    return [line.strip() for line in lines if line.strip() and not line.strip().startswith('#')]
-
 setup(
-    name="1xgpt",
+    name="onexgpt",
     version="1.0",
     packages=find_namespace_packages(include=['genie.*', 'magvit2.*']),
 )
